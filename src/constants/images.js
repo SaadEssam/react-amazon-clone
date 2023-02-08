a001f46b8ccbebf2719a7642e47da97d613b9758
SaadEssam <SaadEssam059@gmail.com>
2023-02-08 16:33:23 +0200

    Categories-section

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,6 +4,29 @@ import subBanner1 from '../assets/subBanner1.jpg';
 import subBanner2 from '../assets/subBanner2.jpg';
 import subBanner3 from '../assets/subBanner3.jpg';
 import subBanner4 from '../assets/subBanner4.jpg';
+import camera from '../assets/camera.png';
+import tablets from '../assets/tablets.png';
+import smartwatchs from '../assets/smartwatchs.png';
+import laptops from '../assets/laptops.png';
+import home from '../assets/home.png';
+import music from '../assets/music.png';
+import ps5 from '../assets/ps5.png';
+import tv from '../assets/tv.png';
 
 
-export default { amazon, mainBanner1, subBanner1, subBanner2, subBanner3, subBanner4 };
+export default { 
+  amazon,
+  mainBanner1, 
+  subBanner1, 
+  subBanner2, 
+  subBanner3, 
+  subBanner4, 
+  laptops,
+  camera,
+  tv,
+  ps5,
+  music,
+  home,
+  smartwatchs,
+  tablets,
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import images from '../../constants/images';
 import './Home.css';
+import { HiOutlineReceiptPercent } from 'react-icons/hi2';
+import { 
+  MdOutlineLocalShipping, 
+  MdOutlineCardGiftcard, 
+  MdOutlineHeadsetMic,
+  MdCreditCard,
+} from 'react-icons/md';
 
 const Home = () => {
   return (
@@ -53,6 +60,118 @@ const Home = () => {
                     <h5>AirPods Max</h5>
                     <p>High-fidelity playback & ultra-low distortion</p>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-15">
+                  <MdOutlineLocalShipping />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all orders over $100</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <MdOutlineCardGiftcard />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p className="mb-0">Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <MdOutlineHeadsetMic />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <HiOutlineReceiptPercent />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get Factory direct price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <MdCreditCard />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Protected Payments</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="categories d-flex align-items-center flex-wrap justify-content-between">
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Cameras</h6>
+                    <p className="mb-0">10 Items</p>
+                  </div>
+                  <img src={images.camera} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Computers & Laptop</h6>
+                    <p className="mb-0">8 Items</p>
+                  </div>
+                  <img src={images.laptops} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Mobiles & Tablets</h6>
+                    <p className="mb-0">14 Items</p>
+                  </div>
+                  <img src={images.tablets} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>smartwatch's</h6>
+                    <p className="mb-0">5 Items</p>
+                  </div>
+                  <img src={images.smartwatchs} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Headphones</h6>
+                    <p className="mb-0">4 Items</p>
+                  </div>
+                  <img src={images.music} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Smart TV</h6>
+                    <p className="mb-0">8 Items</p>
+                  </div>
+                  <img src={images.tv} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Home Appliances</h6>
+                    <p className="mb-0">9 Items</p>
+                  </div>
+                  <img src={images.home} />
+                </div>
+                <div className="d-flex align-items-center gap">
+                  <div>
+                    <h6>Gaming Console</h6>
+                    <p className="mb-0">5 Items</p>
+                  </div>
+                  <img src={images.ps5} />
                 </div>
               </div>
             </div>
