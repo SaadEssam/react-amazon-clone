fa75fea41fbfc025f29d5ec0967c582d2ed2871d
SaadEssam <SaadEssam059@gmail.com>
2023-02-12 18:39:18 +0200

    Meta-component

--- src/pages/OurStore/OurStore.jsx
+++ src/pages/OurStore/OurStore.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BreadCrumb from "../../components/BreadCrumb/BreadCrumb";
+import Meta from "../../components/Meta";
 import './OurStore.css';
 
 const OurStore = () => {
   return (
     <>
+      <Meta title="Our Store" />
       <BreadCrumb title="Our Store" />
     </>
   );
