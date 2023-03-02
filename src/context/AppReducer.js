4d5b648ba2281ab5c591c5efe6709d0efafcfc13
SaadEssam <SaadEssam059@gmail.com>
2023-03-02 14:32:37 +0200

    ContextAPi

---
+++ src/context/AppReducer.js
@@ -0,0 +1,18 @@
+export const initialState = {
+  basket: [],
+  user: null,
+};
+
+const AppReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
+
+export default AppReducer;
