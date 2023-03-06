0cf311e8d166dd6132927ae9266b2c7183806635
SaadEssam <SaadEssam059@gmail.com>
2023-03-06 18:08:27 +0200

    firebase-cloud-functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// App config
+
+const app = express();
+
+// middleware
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+
+app.get("/", (req, res) => res.status(200).send("Hello World!"));
+
+app.post("/checkout/create", async (req, res) => {
+  const total = req.query.total;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+// example endpoint
+// http://127.0.0.1:5001/clone-94d39/us-central1/api
+
+// Listen command
+
+exports.api = functions.https.onRequest(app);
