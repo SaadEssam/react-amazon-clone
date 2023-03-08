1908cb879283974018609daf160687078de92d6d
SaadEssam <SaadEssam059@gmail.com>
2023-03-08 15:31:28 +0200

    Orders-page

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import Meta from "../../components/Meta";
+import { useAuth } from "../../context/GlobalState";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../firebase";
+import Order from "../../components/Order/Order";
 import "./Orders.css";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const [orders, setOrders] = useState({});
+  const { user } = useAuth();
+  useEffect(() => {
+    if (user) {
+      const collRef = collection(db, "users", user?.uid, "orders");
+      const orderedRef = query(collRef, orderBy("created", "desc"));
+      onSnapshot(orderedRef, (querySnapshot) => {
+        setOrders(
+          querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+  return (
+    <>
+      <Meta title="Your Orders" />
+      <div className="orders">
+        <h1>Your Orders</h1>
+        <div className="order">
+          {orders?.map((order) => (
+            <Order order={order} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Orders;
